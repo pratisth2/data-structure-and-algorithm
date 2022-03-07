ac75621ac52302130da7119f29d2c19ec42357f7
PRATISTHA MISHRA <73077949+pratisth2@users.noreply.github.com>
2022-03-07 16:56:42 +0530

    Time: 5 ms (10.62%), Space: 6.3 MB (15.19%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int>dp(n+1,0);
+         int prev=1;
+        int prev2=1;
+        for(int i=2;i<=n;i++)
+        {
+            int curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+    }
+};
