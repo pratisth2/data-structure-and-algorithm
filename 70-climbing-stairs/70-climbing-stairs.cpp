e1d380a48f1fc957f75c486734d3e84d9f9c790f
PRATISTHA MISHRA <73077949+pratisth2@users.noreply.github.com>
2022-03-24 21:58:08 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.47%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -3,12 +3,19 @@ public:
     int climb(int n, vector<int>&dp)
     {
         if(n==0) return 1;
-        if(n<0) return 0;
         if(dp[n]!=-1) return dp[n];
-        return dp[n]=climb(n-1,dp)+climb(n-2,dp);
+        int onestep=0, twostep=0;
+        onestep=climb(n-1,dp);
+        if(n-2>=0) twostep=climb(n-2,dp);
+        return dp[n]=onestep+twostep;
     }
     int climbStairs(int n) {
        vector<int>dp(n+1,-1);
        return climb(n,dp);
     }
+//       int climbStairs(int n) {
+//        vector<int>dp(n+1);
+//        dp[0]=1;   
+       
+//     }
 };
