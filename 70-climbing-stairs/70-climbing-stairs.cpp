335b55fd7e859745fc0fa442c340e50a20838ccf
PRATISTHA MISHRA <73077949+pratisth2@users.noreply.github.com>
2022-03-24 21:43:29 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (29.69%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,27 +2,13 @@ class Solution {
 public:
     int climb(int n, vector<int>&dp)
     {
-        if(n==0)
-        {
-            return 1;
-        }
-          if(n<0)
-          {
-           return 0;
-           }
-        if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
-      //  cout<<"hello"<<endl;
-        int a=climb(n-1,dp);
-        int b=climb(n-2,dp);
-        int c=a+b;
-        dp[n]=c;
-        return c;
+        if(n==0) return 1;
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=climb(n-1,dp)+climb(n-2,dp);
     }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
+       vector<int>dp(n+1,-1);
        return climb(n,dp);
     }
 };
