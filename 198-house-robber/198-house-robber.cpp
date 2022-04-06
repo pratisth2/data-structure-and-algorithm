3e1a60d6d77037d5279e2c0ce8f12194c49cc394
PRATISTHA MISHRA <73077949+pratisth2@users.noreply.github.com>
2022-04-06 17:57:24 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (47.70%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
-    int robutil(int idx, vector<int>&nums, vector<int>&dp)
-    {
-        if(idx==0)
-        {
-            return nums[idx];
-        }
-        if(idx<0)
-        {
-            return 0;
-        }
-        if(dp[idx]!=-1)
-        {
-            return dp[idx];
-        }
-        int pick=nums[idx]+ robutil(idx-2,nums,dp);
-        int notpick=0+robutil(idx-1,nums,dp);
-        return dp[idx]= max(pick, notpick);
-    }
+    // int robutil(int idx, vector<int>&nums, vector<int>&dp)
+    // {
+    //     if(idx==0)
+    //     {
+    //         return nums[idx];
+    //     }
+    //     if(idx<0)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[idx]!=-1)
+    //     {
+    //         return dp[idx];
+    //     }
+    //     int pick=nums[idx]+ robutil(idx-2,nums,dp);
+    //     int notpick=0+robutil(idx-1,nums,dp);
+    //     return dp[idx]= max(pick, notpick);
+    // }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-      return  robutil(n-1, nums,dp);
+        vector<int>dp(n);
+        dp[0]=nums[0];
+        int neg=0;
+        for(int i=1;i<n;i++)
+        {
+            int pick=nums[i];
+            if(i>1)
+            {
+                pick+=dp[i-2];
+            }
+            int notpick=0+dp[i-1];
+            dp[i]=max(pick,notpick);
+        }
+    return dp[n-1];
         
     }
 };
