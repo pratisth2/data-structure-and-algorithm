f1b37183197ea2f2aaf2a932552e4109dcca6b89
PRATISTHA MISHRA <73077949+pratisth2@users.noreply.github.com>
2022-05-28 17:14:21 +0530

    Time: 10 ms (15.31%), Space: 7.6 MB (91.79%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-  void perm(vector<vector<int>>&ans, vector<int>&v, vector<int>&nums, int mark[])
-  {
-      if(v.size()==nums.size())
-      {
-          ans.push_back(v);
-          return;
-      }
-      for(int i=0;i<nums.size();i++)
-      {
-          if(!mark[i])
-          {
-              v.push_back(nums[i]);
-                  mark[i]=1;
-              perm(ans,v,nums,mark);
-          mark[i]=0;
-          v.pop_back();
-      }
-  }
-  }
+    void helper(vector<vector<int>>&ans, int idx, vector<int>&nums)
+    {
+        if(idx>=nums.size())
+        {
+           ans.push_back(nums);
+            return;
+        }
+        for(int i=idx;i<nums.size();i++)
+        {
+            swap(nums[idx],nums[i]);
+            helper(ans,idx+1,nums);
+             swap(nums[idx],nums[i]);
+            
+        }
+    }
    vector<vector<int>> permute(vector<int>& nums) {
        vector<vector<int>>ans;
-       vector<int>v;
-       int mark[nums.size()];
-       for(int i=0;i<nums.size();i++)
-       {
-           mark[i]=0;
-       }
-       perm(ans,v,nums,mark);
+       int idx=0;
+       helper(ans,0,nums);
        return ans;
    }
+     
 };
