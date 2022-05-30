b9ec72f3c04a0c43aea35610b3c712d0ab4a6229
PRATISTHA MISHRA <73077949+pratisth2@users.noreply.github.com>
2022-05-30 13:39:18 +0530

    Added solution - LeetHub

--- Maximum Index - GFG/maximum-index.cpp
+++ Maximum Index - GFG/maximum-index.cpp
@@ -13,8 +13,8 @@ class Solution{
     int maxIndexDiff(int A[], int N) 
     { 
         int ans=0;
-        for(int i=0;i<N-1;i++){
-            for(int j=N-1;j>0;j--){
+        for(int i=0;i<N;i++){
+            for(int j=N-1;j>i;j--){
                 if(A[i]<=A[j]){
                     ans=max(ans,j-i);
                     break;
