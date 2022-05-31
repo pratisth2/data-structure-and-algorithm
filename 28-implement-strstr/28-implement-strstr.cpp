9b89c51e208dfbe570094821738891dca083b1c5
PRATISTHA MISHRA <73077949+pratisth2@users.noreply.github.com>
2022-05-31 14:20:18 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (59.99%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.size()==0)
+        {
+            return 0;
+        }
+        return haystack.find(needle);
+    }
+};
