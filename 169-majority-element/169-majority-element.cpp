984a29461fb7a071f39b8fc63c1a5d277bcebfc8
PRATISTHA MISHRA <73077949+pratisth2@users.noreply.github.com>
2022-05-31 11:39:56 +0530

    Time: 25 ms (58.65%), Space: 19.6 MB (59.79%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,12 +1,16 @@
-class Solution {
+ class Solution {
 public:
-    int maj(vector<int>&nums)
-    {
-        int val=nums[0];
-        int count=1;
-        for(int i=1;i<nums.size();i++)
+   
+    int majorityElement(vector<int>& nums) {
+       int count=0;
+        int cand=0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(val==nums[i])
+            if(count==0)
+            {
+                cand=nums[i];
+            }
+            if(nums[i]==cand)
             {
                 count++;
             }
@@ -14,31 +18,7 @@ public:
             {
                 count--;
             }
-            if(count==0)
-            {
-                val=nums[i];
-                count=1;
-            }
         }
-        return val;
-    }
-    int majorityElement(vector<int>& nums) {
-        int value=maj(nums);
-        int c=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(value==nums[i])
-            {
-                c++;
-            }
-        }
-        if(c>nums.size()/2)
-        {
-            return value;
-        }
-        else
-        {
-            return 0;
-        }
+        return cand;
     }
 };
