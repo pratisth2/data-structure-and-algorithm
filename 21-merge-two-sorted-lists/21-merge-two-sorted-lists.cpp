001b7f98817750266e014615e51806881e18c0aa
PRATISTHA MISHRA <73077949+pratisth2@users.noreply.github.com>
2022-06-09 01:20:06 +0530

    Time: 4 ms (93.64%), Space: 14.9 MB (14.73%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,20 +10,49 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
+     {
+         ListNode* dummy=new ListNode(-1);
+         ListNode* tail=dummy;
+         while(list1!=NULL && list2!=NULL)
+         {
+             if(list1->val<list2->val)
+             {
+                 tail->next=list1;
+                 tail=tail->next;
+                 list1=list1->next;
+             }
+             else
+             {
+                 tail->next=list2;
+                 tail=tail->next;
+                 list2=list2->next;
+             }
+         }
+         if(list1!=NULL)
+         {
+             tail->next=list1;
+         }
+          if(list2!=NULL)
+         {
+             tail->next=list2;
+         }
+         return dummy->next;
+     }
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
      
-       if(list1==NULL) return list2;
-        if(list2==NULL) return list1;
-        if(list1->val <=list2->val)
-        {
-            list1->next=mergeTwoLists(list1->next, list2);
-            return list1;
-        }
-        else
-        {
-            list2->next=mergeTwoLists(list1, list2->next);
-            return list2;
-        }
+//        if(list1==NULL) return list2;
+//         if(list2==NULL) return list1;
+//         if(list1->val <=list2->val)
+//         {
+//             list1->next=mergeTwoLists(list1->next, list2);
+//             return list1;
+//         }
+//         else
+//         {
+//             list2->next=mergeTwoLists(list1, list2->next);
+//             return list2;
+//         }
         
-    }
+//     }
 };
