f35f6cd3483c4eb88df98de01854a719b9f951b5
PRATISTHA MISHRA <73077949+pratisth2@users.noreply.github.com>
2022-06-21 18:30:57 +0530

    Time: 4 ms (73.65%), Space: 12.9 MB (82.02%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -11,38 +11,22 @@
  */
 class Solution {
 public:
-    void path(TreeNode* root, string temp,vector<string>&v)
-    {
-      if(root->left==NULL && root->right==NULL)
-      {
-          stringstream ss;
-          ss<<root->val;
-          string s;
-          ss>>s;
-         temp=temp+s;
-         v.push_back(temp);
-          return ;
-      }
-        stringstream ss;
-        ss<<root->val;
-          string s;
-            ss>>s;
-            temp=temp+s+"->";
-        if(root->left!=NULL)
-        {
-            path(root->left,temp,v);
-        }
-        if(root->right!=NULL)
-        {
-            path(root->right,temp,v);
-        }
+    
+ void binaryTreePaths(vector<string>& result, TreeNode* root, string t) {
+    if(!root->left && !root->right) {
+        result.push_back(t);
         return;
     }
+
+    if(root->left!=NULL)
+        binaryTreePaths(result, root->left, t + "->" + to_string(root->left->val));
+    if(root->right!=NULL) 
+        binaryTreePaths(result, root->right, t + "->" + to_string(root->right->val));
+}
     vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string>v;
-        string temp;
-        path(root,temp,v);
-        return v;
-        
+      vector<string>s;
+        if(!root) return s;
+        binaryTreePaths(s,root,to_string(root->val));
+        return s;
     }
 };
