b5adf84ffd22cf8e210d1d841fa1142484618535
PRATISTHA MISHRA <73077949+pratisth2@users.noreply.github.com>
2022-06-27 16:15:07 +0530

    Time: 11 ms (18.72%), Space: 11.1 MB (28.93%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -2,49 +2,39 @@ class Solution {
 public:
     int binarysearch(vector<int>&nums, int target)
     {
-        int l=0;
-        int r=nums.size()-1;
-        int ans=-1;
-        while(l<=r)
+       int l=0;
+        int h=nums.size()-1;
+        while(l<=h)
         {
-            int mid=l+(r-l)/2;
-            if(target==nums[mid]) 
+            int mid=l+(h-l)/2;
+            if(target==nums[mid]) return mid;
+            if(nums[l] <= nums[mid])
             {
-                return mid;
+                if(target>=nums[l] && target<=nums[mid])
+                {
+                    h=mid-1;
+                }
+                else
+                {
+                 l=mid+1;
+                }
+                
             }
-              else if(target<nums[mid])
-               {
-                   if(nums[mid]<nums[r])
-                   {
-                       r=mid-1;
-                   }
-                   else if(target<nums[l])
-                   {
-                       l=mid+1;
-                   }
-                   else
-                   {
-                       r=mid-1;
-                   }
-               }
-                   else
-                  {
-                 if(nums[mid]>nums[l])
-                   {
-                       l=mid+1;
-                   }
-                   else if(target>nums[r])
-                   {
-                       r=mid-1;
-                   }
-                   else
-                   {
-                       l=mid+1;
-                   }
+            else 
+            {
+                if(target>=nums[mid] && target<= nums[h])
+                {
+                l=mid+1;
+                }
+                else
+                {
+                    h=mid-1;
+                }
             }
         }
         return -1;
     }
+    
            
     int search(vector<int>& nums, int target) {
        return  binarysearch(nums,target);
